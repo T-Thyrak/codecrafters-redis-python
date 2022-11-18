7d480f5b2e224c5f868d7cab0bec5617d517ff74
T-Thyrak <bus.teng.thaisothyrak@gmail.com>
2022-11-18 22:15:39 +0700

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 # Uncomment this to pass the first stage
-# import socket
+import socket
 
 
 def main():
@@ -8,8 +8,8 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # server_socket.accept() # wait for client
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket.accept() # wait for client
 
 
 if __name__ == "__main__":
