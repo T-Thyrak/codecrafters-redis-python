a728026336930651cee6f2805f5a776d92524b2c
T-Thyrak <bus.teng.thaisothyrak@gmail.com>
2022-11-18 22:22:07 +0700

    using send with no checking

--- app/main.py
+++ app/main.py
@@ -9,7 +9,10 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    connection, return_address = server_socket.accept() # wait for client
+    
+    with connection:
+        connection.send("+PONG\r\n")
 
 
 if __name__ == "__main__":
