858b17961c13ccfae0feb9ee15de8e9eff7c9bc0
T-Thyrak <bus.teng.thaisothyrak@gmail.com>
2022-11-19 22:10:54 +0700

    i forgot to recv the data, and now accept in while loop

--- app/main.py
+++ app/main.py
@@ -1,18 +1,15 @@
-# Uncomment this to pass the first stage
 import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, return_address = server_socket.accept() # wait for client
     
-    with connection:
-        connection.send(b"+PONG\r\n")
+    while True:
+        connection, return_address = server_socket.accept() # wait for client
+        
+        with connection:
+            data = connection.recv(1024)
+            connection.send(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
