ad3e77f3e6b24c68db3f4b2d38cffd29a266a43b
T-Thyrak <bus.teng.thaisothyrak@gmail.com>
2022-11-19 22:14:27 +0700

    should not wait for another accept

--- app/main.py
+++ app/main.py
@@ -4,11 +4,10 @@ import socket
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     
+    connection, return_address = server_socket.accept() # wait for client
+    
     while True:
-        connection, return_address = server_socket.accept() # wait for client
-        
         data = connection.recv(1024)
-        
         connection.send(b"+PONG\r\n")
         
     connection.close()
