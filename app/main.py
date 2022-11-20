b712cac585f7a5979a040ef6fe137d4e9d64af93
T-Thyrak <bus.teng.thaisothyrak@gmail.com>
2022-11-20 22:44:18 +0700

    use processes

--- app/main.py
+++ app/main.py
@@ -1,28 +1,22 @@
 import socket
-import asyncio
 import threading
-
-threads = []
-
-async def handle_client(loop: asyncio.AbstractEventLoop, client_socket, address):
-    def func(socket, addr):
-        while True:
-            data = socket.recv(1024)
-            socket.send(b"+PONG\r\n")
-        socket.close()
-    
-    thread = threading.Thread(target=func, args=(client_socket, address))
-    threads.append(thread)
-    
-    thread.start()
+import multiprocessing
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    loop = asyncio.get_event_loop()
+    
+    pool = multiprocessing.Pool()
     
     while True:
         client_socket, address = server_socket.accept()
-        loop.create_task(handle_client(loop, client_socket, address))
+        pool.apply_async(handle_client, args=(client_socket, address))
     
+async def handle_client(socket, addr):
+    while True:
+        data = socket.recv(1024)
+        socket.send(b"+PONG\r\n")
+        
+    socket.close()
+
 if __name__ == "__main__":
     main()
