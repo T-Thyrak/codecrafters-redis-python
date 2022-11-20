503557c2eabb056c49ad1406d820e7ae1f3f0589
T-Thyrak <bus.teng.thaisothyrak@gmail.com>
2022-11-20 22:31:42 +0700

    use asyncio event loop

--- app/main.py
+++ app/main.py
@@ -1,16 +1,22 @@
 import socket
+import asyncio
+
+
+def handle_client(client_socket, address):
+    while True:
+        data = client_socket.recv(1024)
+        client_socket.send(b"+PONG\r")
+    
+    client_socket.close()
 
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    
-    connection, return_address = server_socket.accept() # wait for client
+    loop = asyncio.get_event_loop()
     
     while True:
-        data = connection.recv(1024)
-        connection.send(b"+PONG\r\n")
-        
-    connection.close()
-
+        client_socket, address = server_socket.accept()
+        loop.create_task(handle_client(client_socket, address))
+    
 if __name__ == "__main__":
     main()
