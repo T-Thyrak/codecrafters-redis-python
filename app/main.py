9f6a7a619bdbe753bdcee3e68b746f52150c7418
T-Thyrak <bus.teng.thaisothyrak@gmail.com>
2022-11-23 22:15:17 +0700

    use regex to parse echo data

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import multiprocessing
+import re
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
@@ -14,7 +15,21 @@ def main():
 def handle_client(socket, addr):
     while True:
         data = socket.recv(1024)
-        socket.send(b"+PONG\r\n")
+        # if not data:
+        #     break
+        
+        if data == b"+PING\r\n":
+            socket.send(b"+PONG\r\n")
+        else:
+            str_data = data.decode("utf-8")
+            echo_pattern = r"\*2\r\n\$4\r\nECHO\r\n$(\d+)\r\n(.+)\r\n"
+            match_echo = re.match(echo_pattern, str_data)
+            
+            if match_echo:
+                str_len = int(match_echo.group(1))
+                str_echo = match_echo.group(2)
+                
+                socket.send(bytes(f"${str_len}\r\n{str_echo}\r\n", "utf-8"))
         
     socket.close()
 
