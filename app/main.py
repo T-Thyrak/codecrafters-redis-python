4c82bc4a72d48020fead53e5147f4828315b1bef
T-Thyrak <bus.teng.thaisothyrak@gmail.com>
2022-11-23 22:33:33 +0700

    hopefully fixed

--- app/main.py
+++ app/main.py
@@ -12,25 +12,35 @@ def main():
         client_socket, address = server_socket.accept()
         pool.apply_async(handle_client, args=(client_socket, address))
     
-def handle_client(socket, addr):
+def handle_client(socket: socket.socket, addr: socket._RetAddress):
     while True:
         data = socket.recv(1024)
         # if not data:
         #     break
         
-        if data == b"+PING\r\n":
-            socket.send(b"+PONG\r\n")
-        else:
-            str_data = data.decode("utf-8")
-            echo_pattern = r"\*2\r\n\$4\r\nECHO\r\n$(\d+)\r\n(.+)\r\n"
-            match_echo = re.match(echo_pattern, str_data)
-            
-            if match_echo:
-                str_len = int(match_echo.group(1))
-                str_echo = match_echo.group(2)
+        str_data = data.decode("utf-8")
+        
+        ping_pattern = r"(?i)\*(\d+)\r\n\$4\r\nping\r\n(\$(\d+)\r\n(.*)\r\n)?"
+        match_ping = re.match(ping_pattern, str_data)
+        if match_ping:
+            if match_ping.group(2):
+                str_len = int(match_ping.group(3))
+                str_echo = match_ping.group(4)
                 
                 socket.send(bytes(f"${str_len}\r\n{str_echo}\r\n", "utf-8"))
+            else:
+                socket.send(bytes("+PONG\r\n"))
         
+        echo_pattern = r"(?i)\*2\r\n\$4\r\nECHO\r\n$(\d+)\r\n(.*)\r\n"
+        match_echo = re.match(echo_pattern, str_data)
+        
+        if match_echo:
+            str_len = int(match_echo.group(1))
+            str_echo = match_echo.group(2)
+            
+            socket.send(bytes(f"${str_len}\r\n{str_echo}\r\n", "utf-8"))
+            continue
+    
     socket.close()
 
 if __name__ == "__main__":
